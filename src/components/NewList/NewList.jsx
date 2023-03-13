6e97c099a5b45d5a20cdcd5f093eb7d6e1e9ac63
sornekian <sornekian_sarvornek@gmail.com>
2023-03-13 14:29:52 -0700

    updates key

--- src/components/NewList/NewList.jsx
+++ src/components/NewList/NewList.jsx
@@ -3,12 +3,12 @@ import NewsItem from '../NewsItem/NewItem';
 
 export default function NewsList() {
     const [articles, setArticles] = useState([]);
+    const KEY = process.env.REACT_APP_API_KEY;
 
     useEffect(() => {
         const getArticles = async () => {
             const response = await fetch(
-                'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=559fe344d7be450da5408ca58cf74e73'
-            );
+                `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${KEY}`);
             const data = await response.json();
             setArticles(data.articles);
         };
